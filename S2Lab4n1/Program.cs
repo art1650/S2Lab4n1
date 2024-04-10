60cf08c300fc0bfb1a8437475a3d1cec32fa41ff
vakart <vak04012006.3@gmail.com>
2024-04-10 12:36:55 +0300

    Add project files.

---
+++ S2Lab4n1/Program.cs
@@ -0,0 +1,49 @@
+﻿using S2Lab4n1;
+
+Car lightweightCar = new Car(220, 135, "GTR", "green");
+
+lightweightCar.Write();
+
+Console.WriteLine("бажаєте змінити колір yes=1 no=0");
+int x;
+x = int.Parse(Console.ReadLine());
+
+if (x == 1)
+{
+    Console.WriteLine("яка нова колір");
+    string color = Console.ReadLine();
+    lightweightCar.ChangeColor(lightweightCar, color);
+}
+
+Console.WriteLine("бажаєте змінити марку yes=1 no=0");
+int y;
+y = int.Parse(Console.ReadLine());
+
+if (y == 1)
+{
+    Console.WriteLine("яка нова марка");
+    string marka = Console.ReadLine();
+    lightweightCar.ChangeBrend(lightweightCar, marka);
+}
+
+Console.WriteLine("бажаєте додати грузопідйомність yes=1 no=0");
+int z;
+z = int.Parse(Console.ReadLine());
+if(z == 1)
+{
+    Console.WriteLine("скільки додати?");
+    int plusLoadCapacity;
+    plusLoadCapacity = int.Parse(Console.ReadLine());
+    lightweightCar.ChangeLoadCapacity(lightweightCar, plusLoadCapacity);
+}
+Console.WriteLine("бажаєте змінити вагу?");
+int c;
+c = int.Parse(Console.ReadLine());
+if (c == 1)
+{
+    Console.WriteLine("як змынити вагу");
+    int newweight;
+    newweight = int.Parse(Console.ReadLine());
+    lightweightCar.ChangeWeight(lightweightCar, newweight);
+}
+lightweightCar.Write();
---
+++ S2Lab4n1/Truck.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2Lab4n1
+{
+    public class Truck
+    {
+        public int LoadCapacity;
+        public int Weight;
+
+        public void SetLoadCapacity(int LoadCapacity)
+        {
+            this.LoadCapacity = LoadCapacity;
+        }
+        public int GetLoadCapacity()
+        {
+            return LoadCapacity;
+        }
+        public void SetWeighr(int Weight)
+        {
+            this.Weight = Weight;
+        }
+
+        public override string ToString()
+        {
+            return $"Вантажопідйомність = {LoadCapacity}, вага = {Weight}";
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is Truck truck)
+            return LoadCapacity == truck.LoadCapacity && Weight == truck.Weight;
+            return false;
+        }
+    }
+    public class LightweightCar: Truck
+    {
+        public string Brand;
+
+        public string GetBrend()
+        {
+            return Brand;
+        }
+        public void SetBrend(string Brend)
+        {
+            this.Brand = Brend;
+        }
+
+        public override string ToString()
+        {
+            return $"Вантажопідйомність = {LoadCapacity}, вага = {Weight}, марка - {Brand}";
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is LightweightCar truck)
+                return LoadCapacity == truck.LoadCapacity && Weight == truck.Weight && Brand == truck.Brand;
+            return false;
+        }
+
+        
+       
+    }
+
+    public class Car : LightweightCar
+    {
+       
+        public string Color ;
+
+        public Car(int LoadCapacity, int Weight, string Brand, string Color)
+        {
+            this.LoadCapacity = LoadCapacity;
+            this.Weight = Weight;
+            this.Brand = Brand;
+            this.Color = Color;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is LightweightCar truck)
+                return LoadCapacity == truck.LoadCapacity && Weight == truck.Weight && Brand == truck.Brand && Color == Color;
+            return false;
+        }
+
+        public void SetColor(string Color)
+        {
+            this.Color = Color;
+        }
+
+        public void Write()
+        {
+            Console.WriteLine($"груз на = {LoadCapacity} вага = {Weight} марка - {Brand} колір - {Color}");
+        }
+
+        public void ChangeColor(Car Color, string s )
+        {
+            Color.SetColor(s);
+        }
+        public void ChangeBrend(Car brend, string s)
+        {
+            brend.SetBrend(s);
+        }
+        public void ChangeLoadCapacity(Car LoadCapacity, int x)
+        {
+            LoadCapacity.SetLoadCapacity(x + GetLoadCapacity());
+        }
+        public void ChangeWeight(Car Weight, int x)
+        {
+            Weight.SetWeighr(x);
+        }
+    }
+}
